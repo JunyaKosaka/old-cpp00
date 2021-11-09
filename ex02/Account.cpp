f05e06cee223363023ae2af026b26012fbda37bf
Junya Kosaka <kosakajunya@gmail.com>
2021-11-09 21:06:50 +0900

    ex02

---
+++ ex02/Account.cpp
@@ -0,0 +1,96 @@
+#include "Account.hpp"
+#include <iostream>
+#include <time.h>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) : _amount(initial_deposit) {
+	this->_accountIndex = Account::getNbAccounts();
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex <<";amount:" << this->_amount << ";created\n";
+	Account::_totalAmount += this->_amount;
+	Account::_nbAccounts += 1;
+}
+
+Account::~Account( void ) {
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed\n";
+	Account::_nbAccounts -= 1;
+}
+
+int	Account::getNbAccounts( void ) {
+	return Account::_nbAccounts;
+}
+
+int	Account::getTotalAmount( void ) {
+	return Account::_totalAmount;
+}
+
+int	Account::getNbDeposits( void ) {
+	return Account::_totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals( void ) {
+	return Account::_totalNbWithdrawals;
+}
+
+void	Account::displayAccountsInfos( void ) {
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << Account::getNbAccounts() << ";total:" << Account::getTotalAmount()  \
+		<< ";deposits:" << Account::getNbDeposits() << ";withdrawals:" << Account::getNbWithdrawals() << std::endl;
+	// std::cout << "amount" << std::endl;
+	return ;
+}
+
+void	Account::makeDeposit( int deposit ) { // index amount deposits nb_deposits:
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount;
+	this->_amount += deposit;
+	this->_nbDeposits += 1;
+	Account::_totalNbDeposits += 1;
+	std::cout << ";deposits:" << deposit << ";amount:" << this->_amount << \
+		";nb_deposits:" << this->_nbDeposits;
+	std::cout << std::endl;
+}
+
+bool	Account::makeWithdrawal( int withdrawal ) { // index p_amount ;withdrawal: amount nb_withdrawals:1
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:";
+	if (this->_amount < withdrawal) {
+		std::cout <<"refused" << std::endl;
+		return 0;
+	}
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals += 1;
+	Account::_totalNbWithdrawals += 1;
+	std::cout << withdrawal << ";amount:" << this->_amount << \
+		";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+	return 1;
+}
+
+int		Account::checkAmount( void ) const {
+	return _totalAmount;
+}
+
+void	Account::displayStatus( void ) const { // 個別 index:0;amount:42;deposits:0;withdrawals:0
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount \
+		<< ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+}
+
+void	Account::_displayTimestamp( void ) {
+	struct tm tm;
+	time_t t = time(NULL);
+	localtime_r(&t, &tm);
+	std::cout << "[" << tm.tm_year + 1900 << tm.tm_mon + 1 << tm.tm_mday << \
+		"_" << tm.tm_hour << tm.tm_min << tm.tm_sec << "]";
+}
+
+Account::Account(void) {
+	(void) _nbDeposits;
+	(void) _nbWithdrawals;
+	;
+}
---
+++ ex02/Account.hpp
@@ -0,0 +1,69 @@
+// ************************************************************************** //
+//                                                                            //
+//                Account.hpp for GlobalBanksters United                //
+//                Created on  : Thu Nov 20 19:43:15 1989                      //
+//                Last update : Wed Jan 04 14:54:06 1992                      //
+//                Made by : Brad "Buddy" McLane <bm@gbu.com>                  //
+//                                                                            //
+// ************************************************************************** //
+
+
+#pragma once
+#ifndef __ACCOUNT_H__
+#define __ACCOUNT_H__
+
+// ************************************************************************** //
+//                               Account Class                                //
+// ************************************************************************** //
+
+class Account {
+
+
+public:
+
+	typedef Account		t;
+
+	static int	getNbAccounts( void );
+	static int	getTotalAmount( void );
+	static int	getNbDeposits( void );
+	static int	getNbWithdrawals( void );
+	static void	displayAccountsInfos( void );
+
+	Account( int initial_deposit );
+	~Account( void );
+
+	void	makeDeposit( int deposit );
+	bool	makeWithdrawal( int withdrawal );
+	int		checkAmount( void ) const;
+	void	displayStatus( void ) const;
+
+
+private:
+
+	static int	_nbAccounts;  // Accountがいくつあるか
+	static int	_totalAmount;
+	static int	_totalNbDeposits;
+	static int	_totalNbWithdrawals;
+
+	static void	_displayTimestamp( void );
+
+	int				_accountIndex;
+	int				_amount; // 預金高
+	int				_nbDeposits; // 預金回数
+	int				_nbWithdrawals; // 引き出し回数
+
+	Account( void );
+
+};
+
+
+
+// ************************************************************************** //
+// vim: set ts=4 sw=4 tw=80 noexpandtab:                                      //
+// -*- indent-tabs-mode:t;                                                   -*-
+// -*- mode: c++-mode;                                                       -*-
+// -*- fill-column: 75; comment-column: 75;                                  -*-
+// ************************************************************************** //
+
+
+#endif /* __ACCOUNT_H__ */
